a1011542b33251926fcd2cf8197abf6519cc9764
Logan-Kroeger <lk2336@gmail.com>
2023-01-16 09:31:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.list_app',
+  appName: 'list_app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
