2223eda3a17f2cc8eae94f6b0eebed5067c157e1
Logan-Kroeger <lk2336@gmail.com>
2023-01-16 15:06:02 +0200

    Edit button created

--- myApp/src/pages/Home.tsx
+++ myApp/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonProgressBar, IonItem, IonLabel, IonCheckbox, IonButton, IonIcon } from '@ionic/react';
-import { home, add, trash } from 'ionicons/icons';
+import { home, add, trash, create } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -19,10 +19,13 @@ const Home: React.FC = () => {
       <IonItem>
         <IonCheckbox slot="start"/>
         <IonLabel>Task Name</IonLabel>
-        <IonButton slot="end" color='secondary'>
+        <IonButton color='primary'>
           <IonIcon slot="icon-only" icon={add}></IonIcon>
         </IonButton>
-        <IonButton slot="end" color='danger'>
+        <IonButton color='secondary'>
+          <IonIcon slot="icon-only" icon={create}></IonIcon>
+        </IonButton>
+        <IonButton color='danger'>
           <IonIcon slot="icon-only" icon={trash}></IonIcon>
         </IonButton>
       </IonItem>
