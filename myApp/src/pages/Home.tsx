85bc33e364f2dbd629b3d770cab1dafe08020e64
Logan-Kroeger <lk2336@gmail.com>
2023-01-16 11:54:57 +0200

    List item created

--- myApp/src/pages/Home.tsx
+++ myApp/src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonInput, IonItem, IonLabel, IonCheckbox, IonButton, IonIcon } from '@ionic/react';
+import { home, add, trash } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -10,13 +11,30 @@ const Home: React.FC = () => {
           <IonTitle>Blank</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+      
+      <IonItem>
+        <IonLabel>Checkbox</IonLabel>
+        <IonButton slot="end">
+          <IonIcon slot="icon-only" icon={trash}></IonIcon>
+        </IonButton>
+        <IonButton slot="end">
+          <IonIcon slot="icon-only" icon={add}></IonIcon>
+        </IonButton>
+        <IonCheckbox slot="end"></IonCheckbox>
+      </IonItem>
+
+      <IonItem>
+        <IonCheckbox slot="start"/>
+        <IonLabel>Task Name</IonLabel>
+        <IonButton slot="end">
+          <IonIcon slot="icon-only" icon={add}></IonIcon>
+        </IonButton>
+        <IonButton slot="end" color='danger'>
+          <IonIcon slot="icon-only" icon={trash}></IonIcon>
+        </IonButton>
+      </IonItem>
+
       </IonContent>
     </IonPage>
   );
